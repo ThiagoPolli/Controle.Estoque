94ed3badc93c12c5baa011e41dd7fa11a65c27da
ThiagoPolli <thiagopolli.palma@gmail.com>
2022-09-26 21:32:17 -0300

    Atualizado banco

---
+++ Controle.Estoque/Migrations/20220927002936_AtualizarBanco.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Controle.Estoque.Migrations
+{
+    public partial class AtualizarBanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
